3195ea71a32174e5d952ef86e3d225f0e5f1da6d
RANAS7 <deepakrana.dr537@gmail.com>
2024-07-18 19:01:38 +0545

    Spring boot project

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,8 +2,12 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("net.javaguides.springboot.model")
+@EnableJpaRepositories("net.javaguides.springboot.repository")
 public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
